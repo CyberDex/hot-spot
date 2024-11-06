bef8fd4d0e4d2a41255edf79b50aa84e340483cc
Dmytro Soldatov <cyberdexx@gmail.com>
2024-11-06 17:05:44 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
+import appData from './package.json';
 
-export default defineConfig(() => ({
-    base: '/',
-    resolve: {
-        alias: {
-            plugins: '/src/plugins',
-            utils: '/src/utils',
-            conf: '/src/conf',
+export default defineConfig(({ command, mode }) => {
+    console.log(
+        `%c 🐳 ${command}: ${appData.name} | ${appData.version} | ${mode}`,
+        'color:green; font-size:20px;',
+    );
+
+    const isDev = mode === 'development';
+
+    return {
+        base: isDev ? '/' : '/hot-spot',
+        resolve: {
+            alias: {
+                plugins: '/src/plugins',
+                utils: '/src/utils',
+                conf: '/src/conf',
+            },
         },
-    },
-}));
+    };
+});
